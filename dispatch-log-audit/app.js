1bfc2f41ebeeaf32913d7ee9c1d5b7930b3ca1df
Keenan Solis <k.leonardsolis@gmail.com>
2019-09-11 06:13:43 -0400

    Tried installing ESLint

--- dispatch-log-audit/app.js
+++ dispatch-log-audit/app.js
@@ -5,10 +5,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 
+
 // ROUTES
 const employeeRoutes = require('./routes/employee/employee');
 const adminRoutes = require('./routes/admin/admin');
 const errorRoutes = require('./routes/error/404');
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +23,7 @@ app.use(errorRoutes);
 // DB CONNECTION
 const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PW}@webdev-cluster-kls-qduay.mongodb.net/test?retryWrites=true&w=majority`;
 
-mongoose.connect(connectionString, { useNewUrlParser: true, dbName: "BuildWebApp_DispatchLogAudit", useFindAndModify: false })
+mongoose.connect(connectionString, { useNewUrlParser: true, dbName: 'BuildWebApp_DispatchLogAudit', useFindAndModify: false })
     .then(() => {
         console.log("SUCCESSFULLY CONNECTED TO MONGO DB ATLAS CLUSTER.")
         app.listen(3000, () => console.log("Server Running: Dispatch Log Audit"));
