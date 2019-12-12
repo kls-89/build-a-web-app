90519b8e9e4376592321534d17740fdafa6b1a54
Keenan Solis <k.leonardsolis@gmail.com>
2019-12-12 13:23:41 -0500

    Flushed out sign up and login forms.

--- react-sound-board/src/pages/auth/login.js
+++ react-sound-board/src/pages/auth/login.js
@@ -2,9 +2,19 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <h2>This is the login page</h2>
-    </div>
+    <>
+      <h1>Login</h1>
+      <h3>Login to access your saved soundboards.</h3>
+      <form method="POST" action="/login">
+        <label htmlFor="username">Username: </label>
+        <input type="text" id="username" name="username" />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <button>Login</button>
+      </form>
+    </>
   );
 };
 
--- react-sound-board/src/pages/auth/signup.js
+++ react-sound-board/src/pages/auth/signup.js
@@ -2,9 +2,28 @@ import React from "react";
 
 const SignUp = () => {
   return (
-    <div>
-      <h2>This is the Sign Up page</h2>
-    </div>
+    <>
+      <h1>Sign Up</h1>
+      <h3>
+        Create an account to save your soundboards and share sounds with your
+        friends!
+      </h3>
+      <form method="POST" action="/signup">
+        <label htmlFor="username">Username: </label>
+        <input type="text" id="username" name="username" />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <label htmlFor="confirmPassword">Confirm Password: </label>
+        <input type="password" id="confirmPassword" name="confirmPassword" />
+        <br />
+        <label htmlFor="email">E-mail: </label>
+        <input type="email" id="email" name="email" />
+        <br />
+        <button>Login</button>
+      </form>
+    </>
   );
 };
 
