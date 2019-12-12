8c34f8cee15caef8b2b70ebd9b98346c81828156
Keenan Solis <k.leonardsolis@gmail.com>
2019-12-12 13:26:22 -0500

    Started thinking about how to implement record/playback functionality

--- react-sound-board/src/pages/sound/new.js
+++ react-sound-board/src/pages/sound/new.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const New = () => {
+  const [recordingInProgress, setRecordingInProgress] = useState(false);
+  const buttonHandler = btn => {
+    console.log(`${btn} button clicked`);
+  };
+
+  const recordButtonStartHandler = event => {
+    setRecordingInProgress(true);
+    console.log("recording started");
+  };
+
+  const recordButtonEndHandler = event => {
+    // setRecordingInProgress(true);
+    console.log("recording ended");
+  };
   return (
-    <div>
+    <>
       <h1>New Sound page</h1>
+      <button
+        onMouseDown={recordButtonStartHandler}
+        onMouseUp={recordButtonEndHandler}
+      >
+        Record Sound
+      </button>
+      <button disabled={!recordingInProgress} onClick={buttonHandler("play")}>
+        Play Sound
+      </button>
+      <button onClick={buttonHandler("submit")}>Record Button</button>
       {/* <input type="file" accept="audio/*" capture /> */}
-    </div>
+    </>
   );
 };
 
